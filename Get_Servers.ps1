939d248f1ee6a64f3a348a482485e5e714d35642
Anthony <Bastion-@users.noreply.github.com>
2018-07-03 09:17:17 -0700

    script can return staff and app info
    The script will now return the staff and applications for each server using the -full flag. Also, added a test to the script.

--- Get_Servers.ps1
+++ Get_Servers.ps1
@@ -3,7 +3,7 @@
 Name: Server Information Retrieval Script
 Author:	Anthony Dunaway
 Date: 06/15/18
-Updated: 06/27/18
+Updated: 07/03/18
 Description:
 Helper script to grab our list of servers and their critical status.
 If critical flag is passed returns a hashtable with the server name as the key 
@@ -13,6 +13,7 @@ and the critical status as the value. Otherwise returns a list of the servers
 Function Get-ServerList{
 	[CmdletBinding()]
 	param(
+		[switch] $full,
 		[switch] $critical,
 		[string] $file_path
 	)
@@ -27,15 +28,32 @@ Function Get-ServerList{
     $servers = $server_db.worksheets.item(1)
     $server_count = ($servers.UsedRange.Rows).count
 	Write-Verbose "Building the server list"
-	if($critical -eq $true){
+	if($full -eq $true){
 		For($line=2; $line -le $server_count - 1; $line++){
-				$current_server = $servers.Cells.Item($line,"A").Value()
-				$current_server = $current_server.ToString().Trim()
-				Write-Debug "Current server : $current_server"
-				$server_is_critical = $servers.Cells.Item($line,"F").Value()
-				Write-Debug "Critical Status : $server_is_critical"
-				$all_servers_dict.add($current_server, $server_is_critical)
+			$server_data = @()
+			$current_server = $servers.Cells.Item($line,"A").Value().ToString().Trim()
+			Write-Debug "Current server : $current_server"
+			$server_is_critical = $servers.Cells.Item($line,"F").Value().ToString().Trim()
+			Write-Debug "Critical Status : $server_is_critical"
+			$staff = $servers.Cells.Item($line,"H").Value().ToString().Trim()
+			Write-Debug "Staff are $staff"
+			$applications = $servers.Cells.Item($line,"I").Value().ToString().Trim()
+			Write-Debug "Applications include $applications"
+			$server_data += $server_is_critical
+			$server_data += $staff
+			$server_data += $applications
+			$all_servers_dict.add($current_server, $server_data)
 		} 
+	}
+	elseif($critical -eq $true){
+		For($line=2; $line -le $server_count - 1; $line++){
+			$current_server = $servers.Cells.Item($line,"A").Value()
+			$current_server = $current_server.ToString().Trim()
+			Write-Debug "Current server : $current_server"
+			$server_is_critical = $servers.Cells.Item($line,"F").Value()
+			Write-Debug "Critical Status : $server_is_critical"
+			$all_servers_dict.add($current_server, $server_is_critical)
+		}
 	}
 	else{
 		For($line=2; $line -le $server_count - 1; $line++){
@@ -45,8 +63,6 @@ Function Get-ServerList{
 				$all_servers += $current_server
 		} 
 	}
-	$number_servers = $all_servers.count
-	Write-Verbose "Number of Servers Found : $number_servers"
 	Write-Verbose "Closing Excel and Cleaning Up"
     $excel.Workbooks.Close()
     $excel.Quit()
@@ -61,3 +77,14 @@ Function Get-ServerList{
 		Return $all_servers
 	}
 }#Function
+
+#test the functionality
+# $test = Get-ServerList -verbose -full -file_path "I:\ISE\CommVault\CommVaultScript\"
+# $test = Get-ServerList -verbose -critical -file_path "I:\ISE\CommVault\CommVaultScript\"
+# $test = Get-ServerList -verbose -file_path "I:\ISE\CommVault\CommVaultScript\"
+# foreach($server in $test.keys){
+	# Write-Verbose $server
+	# foreach($stat in $test[$server]){
+		# Write-Verbose $stat
+	# }
+# }
