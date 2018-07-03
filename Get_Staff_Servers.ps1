4a1ccf00d3922a08f67dcca5357eea254d36b2d2
Anthony <Bastion-@users.noreply.github.com>
2018-07-03 09:20:24 -0700

    Script now exports a csv of staff and their servers
    The file that this script used to read was merged with another file, but the format of that file could still be of use. This script now exports data from the merged file into a csv organized by staff rather than server. The CSV key is staff and the value is all of the servers that staff member is responsible for.

--- Get_Staff_Servers.ps1
+++ Get_Staff_Servers.ps1
@@ -3,47 +3,63 @@
 Name: Get Server Owner List
 Author:	Anthony Dunaway
 Date: 06/15/18
-Updated: 06/18/18
+Updated: 06/29/18
 Description:
-Helper script to get a list of servers and their staff. 
-Returns a hash table with the staff member as the key and 
-a list of their servers as the value.
+Creates a CSV of each team member and their servers
 -----------------------------------------------------------------------
 #>
-Function Get-StaffServers{
-	[CmdletBinding()]
-	param()
-	Write-Verbose "Opening Excel"
-	$file_path = Get-Location
-	$excel = New-Object -comobject Excel.Application -Verbose:$false
-    $excel.DisplayAlerts = $False
-	Write-Verbose "Opening the list of staff"
-	$staff_server_list = $excel.Workbooks.Open("$file_path\StaffServerList.xlsx" )
-	$staff = $staff_server_list.worksheets.item(1)
-	$staff_list = @{}
-	$staff_count = ($staff.UsedRange.Rows).count
-	Write-Verbose "Number of staff Found : $staff_count"
-	For($row = 2; $row -le $staff_count; $row++){
-		$current_owner = $staff.Cells.Item($row, "A").Value().ToString()
-		Write-Debug "Current owner : $current_owner"
-		$current_servers = @()
-		$staff_range = $staff.UsedRange.Cells
-		$column_count = $staff_range.Columns.Count
-		For ($col = 2; $col -le $column_count; $col++){
-			If(-Not [string]::IsNullOrEmpty($staff.Cells.Item($row, $col).Value())){
-				$current_servers += $staff.Cells.Item($row, $col).Value()
-				Write-Debug "Current server is $staff.Cells.Item($row, $col).Value()"
+[CmdletBinding()]
+param(
+	[string] $export_path = "none"
+)
+
+$file_path = "I:\ISE\CommVault\CommVaultScript"
+
+Write-Verbose "Opening Excel"
+$excel = New-Object -comobject Excel.Application -Verbose:$false
+$excel.DisplayAlerts = $False
+Write-Verbose "Opening the server list"
+$server_list = $excel.Workbooks.Open("$file_path\ServerList.xlsx" )
+$worksheet = $server_list.worksheets.item(1)
+$staff_list = @{}
+$num_rows = ($worksheet.UsedRange.Rows).Count
+#Get a list of all the staff
+for($row = 2; $row -le $num_rows; $row++){
+	if(-Not[string]::IsNullOrWhitespace($worksheet.Cells.Item($row, 'H').Value())){
+		$current_staff = $worksheet.Cells.Item($row, 'H').Value().ToString().Trim()
+	}
+	$all_staff += "$current_staff,"
+}
+$all_staff = $all_staff.Split(",")
+$all_staff = $all_staff | Select-Object -Unique
+#find out which servers belong to each staff
+foreach($person in $all_staff){
+	Write-Verbose "Finding the servers for $person "
+	$servers = @()
+	for($row = 2; $row -le $num_rows; $row++){
+		if(-Not[string]::IsNullOrWhitespace($worksheet.Cells.Item($row, 'H').Value())){
+			$current_staff = $worksheet.Cells.Item($row, 'H').Value().ToString().Trim().Split(",")
+			if($current_staff.Contains($person)){
+				$servers += $worksheet.Cells.Item($row, 'A').Value().ToString().Trim()
 			}
 		}
-		Write-Debug "Adding $current_owner to the list"
-		$staff_list.add($current_owner, $current_servers)
 	}
-	Write-Verbose "Closing Excel and Cleaning Up"
-	$excel.Workbooks.Close()
-    $excel.Quit()
-    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($staff)
-    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($staff_server_list)
-    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel)
-	Write-Verbose "List Complete"
-	Return $staff_list
+	$servers = $servers -join ","
+	$staff_list.add($person, $servers)
 }
+Write-Verbose "List Complete"
+Write-Verbose "Closing Excel and Cleaning Up"
+#close Excel
+$server_list.Close()
+$excel.Quit()
+#Export hash table to CSV at the specified directory
+if($export_path -ne "none"){
+	Write-Verbose "Exporting list to CSV at $export_path"
+	$staff_list.GetEnumerator() | Select-Object -Property Key,Value | Export-Csv -NoTypeInformation -path "$export_path\staff_servers.csv"
+}
+#no directory provided export to location of serverlist.xlsx
+else{
+	Write-Verbose "Exporting list to CSV at $file_path"
+	$staff_list.GetEnumerator() | Select-Object -Property Key,Value | Export-Csv -NoTypeInformation -path "$file_path\staff_servers.csv"
+}
+
