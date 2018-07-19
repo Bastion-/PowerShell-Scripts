5722c06dbe0c64a6a80970292f33b9a113610834
Anthony <Bastion-@users.noreply.github.com>
2018-07-19 11:00:59 -0700

    Add files via upload
    Added a new Server object function and a function to return a list of our servers as new server objects

---
+++ Create_Server.ps1
@@ -0,0 +1,47 @@
+<#
+-----------------------------------------------------------------------
+Name: Create Server Object
+Author:	Anthony Dunaway
+Date: 07/19/18
+Updated: 07/19/18
+Description:
+Creates a new server object for use in the CommVault script
+-----------------------------------------------------------------------
+#>
+
+function New-Server {
+	[cmdletbinding()]
+	param(
+		[Parameter(Mandatory=$True)]
+		[string]$name,
+		[Parameter(Mandatory=$True)]
+		[int]$critical,
+		[Parameter(Mandatory=$True)]
+		[int]$appdb,
+		[Parameter(Mandatory=$True)]
+		[string[]]$staff, 
+		[Parameter(Mandatory=$True)]
+		[string[]]$applications
+	)
+	
+	New-Object psobject -property @{
+		Name = $name
+		Critical = $critical
+		AppDB = $appdb
+		Staff = $staff
+		Applications = $applications
+	}
+}
+
+#Testing
+
+# $staff = @("Ophelia.PandaQueen", "Melchior.PandaKing")
+# $apps = @("panda cam", "panda time")
+
+# $current_server = New-Server -name "happypanda" -critical 1 -appdb 0 -staff $staff -applications $apps
+
+# $current_server.Name
+# $current_server.Critical
+# $current_server.AppDB
+# $current_server.Staff
+# $current_server.Applications
---
+++ Get_Server_Objects.ps1
@@ -0,0 +1,66 @@
+<#
+-----------------------------------------------------------------------
+Name: Server Information Retrieval Script
+Author:	Anthony Dunaway
+Date: 06/15/18
+Updated: 07/19/18
+Description:
+Helper script to grab our list of servers.
+Returns a list of Server objects
+-----------------------------------------------------------------------
+#>
+
+$script_path = $PSScriptRoot.ToString()
+."$script_path\Create_Server.ps1"
+
+Function Get-ServerList{
+	[CmdletBinding()]
+	param(
+		[string] $file_path
+	)
+	$all_servers = @()
+	Write-Verbose "Opening Excel"
+    $excel = New-Object -comobject Excel.Application -Verbose:$false
+    $excel.DisplayAlerts = $False
+    $server_db_name = "ServerList.xlsx"
+	Write-Verbose "Opening the Server List"
+    $server_db = $excel.Workbooks.Open("$file_path\$server_db_name")
+    $servers = $server_db.worksheets.item(1)
+    $server_count = ($servers.UsedRange.Rows).count
+	Write-Verbose "Building the server list"
+	For($line=2; $line -le $server_count - 1; $line++){
+		$current_server = $servers.Cells.Item($line,"A").Value().ToString().Trim()
+		$critical = $servers.Cells.Item($line,"F").Value().ToString().Trim()
+		$appdb = $servers.Cells.Item($line,"G").Value()
+		$staff = $servers.Cells.Item($line,"H").Value().ToString().Trim().Split(",")
+		$apps = $servers.Cells.Item($line,"I").Value().ToString().Trim().Split(",")
+		$all_servers += New-Server -name $current_server -critical $critical -appdb $appdb -staff $staff -applications $apps
+	}
+	Write-Verbose "Closing Excel and Cleaning Up"
+    $excel.Workbooks.Close()
+    $excel.Quit()
+    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($servers)
+    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($server_db)
+    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel)
+	[GC]::Collect()
+	Write-Verbose "List Complete"
+	if(($critical -eq $True) -or ($full -eq $True)){
+		Return $all_servers_dict
+	}
+	else{
+		Return $all_servers
+	}
+}
+
+#test the functionality
+# $test = Get-ServerList -verbose -file_path "C:\Development\CommVaultScriptDev\"
+
+# $find_ob = $test | Where-Object {$_.Name -eq "lrn_prod"}
+# $find_ob
+# foreach($server in $test){
+	# Write-Host $server.Name
+	# Write-Host $server.Critical
+	# Write-Host $server.AppDB
+	# Write-Host $server.Staff
+	# Write-Host $server.Applications
+# }
