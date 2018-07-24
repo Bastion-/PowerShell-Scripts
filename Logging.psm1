3659c9188bb519ce412c2c147d471f86dee6e1df
Anthony <Bastion-@users.noreply.github.com>
2018-07-24 10:34:37 -0700

    PowerShell Logging Module
    Module to add simple logging to PowerShell. Start a log, stop a log, add an entry to the log, and send the log via email. Automatically names the log file the name of the calling script with the suffix _log.txt.

---
+++ Logging.psm1
@@ -0,0 +1,225 @@
+<#
+-----------------------------------------------------------------------
+Name: Logging Module
+Author:	Anthony Dunaway
+Date: 07/20/18
+Updated: 07/24/18
+Description:
+Logging Module
+-----------------------------------------------------------------------
+#>
+
+#Returns a PSCredential object. Requires a text file with an encrypted password. PRIVATE
+function Get-MyCredentials(){
+	[cmdletbinding()]
+	param(
+		[string]$filepath = "C:\Development\Password_Files",
+		[string]$username = "username",
+		[string]$filename = "password.txt"
+	)
+	
+	$password = get-content "$filepath\$filename" | convertto-securestring
+	$credentials = new-object System.Management.Automation.PSCredential($username, $password)
+	Return $credentials
+	
+}
+
+#Returns the name of the calling script. PRIVATE
+function Get-CallingScriptName(){
+	[cmdletbinding()]
+	param()
+	$calling_script_path = Get-Item (Get-PSCallStack)[(Get-PSCallStack).length-2].ScriptName
+	$script = $calling_script_path.ToString().Split("\")
+	$script = $script[$script.Length - 1]
+	$script = $script.Split(".")
+	$script = $script[0] + "_log.txt"
+	Return $script	
+}
+
+#Create a new log file. PRIVATE
+function New-LogFile(){
+	[cmdletbinding()]
+	param(
+		[string] $path =  [Environment]::GetFolderPath("MyDocuments")
+	)
+	$script = Get-CallingScriptName
+	if(!(Test-Path -Path "$path\$script")){
+		New-Item -Path $path -Name $script | Out-Null 
+	}
+}
+
+#Start the log with informational header
+function Start-Log(){
+<#
+.SYNOPSIS
+	Start a log file
+.DESCRIPTION
+	Check if log file exists. If not, create one and initialize it with the name of the calling script and date.
+	The file name will be the name of the calling script _log.txt
+.PARAMETER path
+	Location where you would like the log file saved. Default is MyDocuments.
+.EXAMPLE
+	Start-Log -path "C:\Development\"
+#>
+	[cmdletbinding()]
+	param(
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[string]$path
+	)
+	$script = Get-CallingScriptName
+	if($path){
+		New-LogFile -path $path
+	}
+	else{
+		New-LogFile 
+		$path = [Environment]::GetFolderPath("MyDocuments")
+	}
+	$date = Get-Date -UFormat "%A %H:%M"
+	$content = "----------------------------------------------------------------------------------------
+Date: $date
+Beginning of $script 
+----------------------------------------------------------------------------------------
+
+"
+	Add-Content -Path "$path\$script" -Value $content
+}
+
+#Stop the log with informational footer
+function Stop-Log(){
+<#
+.SYNOPSIS
+	Adds a footer to the log file with the date and name of the calling script.
+.PARAMETER path
+	Location of the log file you would like to add a stop footer to. Defualt is MyDocuments.
+.EXAMPLE
+	Stop-Log -path "C:\Development\"
+#>
+	[cmdletbinding()]
+	param(
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[string]$path = [Environment]::GetFolderPath("MyDocuments")
+	)	
+	$script = Get-CallingScriptName	
+	$date = Get-Date -UFormat "%A %H:%M"
+	$content = "
+	
+----------------------------------------------------------------------------------------
+Date: $date
+End of $script
+----------------------------------------------------------------------------------------
+"	
+	Add-Content -Path "$path\$script" -Value $content
+}
+
+function Add-LogEntry(){
+<#
+.SYNOPSIS
+	Adds an entry to the log file.
+.PARAMETER info
+	Switch, if used prefixes INFORMATION: to the entry.
+.PARAMETER error
+	Switch, if used prefixes ERROR: to the entry.
+.PARAMETER warning
+	Switch, if used prefixes WARNING: to the entry.
+.PARAMETER debugging
+	Switch, if used prefixes DEBUG: to the entry.
+.PARAMETER path
+	Location of the log file to add an entry to
+.PARAMETER content
+	The text to be written to the log file.
+.EXAMPLE
+	Add-LogEntry -path "C:\Development\" -info -content "This will be written to the log file"
+#>
+	[cmdletbinding()]
+	param(
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[switch]$info,
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[switch]$error,
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[switch]$warning,
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[switch]$debugging,
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[string]$path = [Environment]::GetFolderPath("MyDocuments"),
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[string]$content
+	)	
+	$script = Get-CallingScriptName	
+	if($info){
+		$prefix = "INFORMATION: "
+	}
+	elseif($error){
+		$prefix = "ERROR: "
+	}
+	elseif($warning){
+		$prefix = "WARNING: "
+	}
+	elseif($debugging){
+		$prefix = "DEBUG: "
+	}
+	else{
+		$prefix = ""
+	}
+	$content = $prefix + $content	
+	Add-Content -Path "$path\$script" -Value $content
+}
+
+function Send-Log(){
+<#
+.SYNOPSIS
+	Sends the log file via email.
+.PARAMETER from
+	Email address of sender
+.PARAMETER to
+	Email address of recipient
+.PARAMETER attach
+	Path to the log file to be sent as an attachment
+.PARAMETER gmail
+	Switch, if used email is sent via Gmail rather than Outlook
+.EXAMPLE
+	Send-Log -from "user@domain.com" -to "user@domain.com" -attach "C:\Development\script_log.txt" -gmail
+.NOTES
+	Requires Get_Credentials.ps1 function Get-MyCredentials. Also requires firewall access to port 587
+	If you want to use the Get-MyCredentials function you will need to provide the path to your 
+	encrypted password file. 
+#>
+	[cmdletbinding()]
+	param(
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[string]$from,
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[string]$to,
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[string]$attach,
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[string]$username = "username",
+		[parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+		[switch]$gmail
+	)
+	
+	if($gmail){
+		$credential = Get-MyCredentials -username $username -filename "gmail.txt"
+		$server = "smtp.gmail.com"
+	}
+	else{
+		$credential = Get-MyCredentials -username $username
+		$server = "smtp_mail.outlook.com"
+	}
+	
+	$script = Get-CallingScriptName
+	
+	$message = @{
+		SmtpServer = $server
+		Credential = $credential
+		Port = "587"
+		UseSsl = $true
+		From = $from
+		To = $to
+		Subject = "$script Log File Report"
+		Body = "Attached is the log file for $script"
+		Attachments = $attach
+	}
+	Send-MailMessage @message
+	
+}
